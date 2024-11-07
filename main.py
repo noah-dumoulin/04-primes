dcfe349d73f68a1b4e5b05fa7482f5b92d1daaf5
noah-dumoulin <noah.dumoulin@edu.esiee.fr>
2024-11-07 09:36:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,21 @@ from math import sqrt
 
 def isprime(p):
 
-    # votre code ici
+    
+premier =True
+
+s=int(sqrt(p))
+for i in range (2,s+1):
+    reste=p%i
+    if reste==0:
+        premier=False
+        diviseur=i
+        m=int(p/i)
+        break
+if premier ==False:
+    return False
+else:
+    return True
 
     pass
 
@@ -15,7 +29,9 @@ def isprime(p):
 def main():
 
     # vos appels à la fonction secondaire ici
-
+    prime(19)
+    prime(9)
+    prime(55)
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
