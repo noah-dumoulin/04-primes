bb13c80afb2fde55fcfa4b1a67464a59fc143ae7
noah-dumoulin <noah.dumoulin@edu.esiee.fr>
2024-11-07 09:31:29 +0100

    primes main.py

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ from math import sqrt
 
 def isprime(p):
 
-    # votre code ici
+    
+premier =True
+s=int(sqrt(p))
+for i in range (2,s+1):
+    reste=p%i
+    if reste==0:
+        premier=False
+        diviseur=i
+        m=int(p/i)
+        break
+if premier ==False:
+    print(p,"=", diviseur, "x", m, ": False")
+else:
+    print(p, ": True")
 
     pass
 
@@ -13,9 +26,11 @@ def isprime(p):
 
 
 def main():
-
+    isprime(5)
+    isprime(19)
+    isprime(21)
     # vos appels à la fonction secondaire ici
-
+    
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
